9d8f3bf78ba6db095edf1c5876a84c0c641f7307
KasK0109 <kaare3014@gmail.com>
2021-09-21 16:17:17 +0200

    -=-

---
+++ src/crapsgame/CrapsApp.java
@@ -0,0 +1,76 @@
+package crapsgame;
+
+import java.util.Scanner;
+
+public class CrapsApp {
+    public static void main(String[] args) {
+        // Create the game objects
+        Die die1 = new Die();
+        Die die2 = new Die();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+
+            // Roll the dice for the first time
+            System.out.println("Start of the game!");
+
+            System.out.println("Press [Enter] to roll the dice");
+            scan.nextLine();
+            int roll1 = die1.roll();
+            int roll2 = die2.roll();
+            int rollTotal = roll1 + roll2;
+            System.out.println("Dice1: " + roll1);
+            System.out.println("Dice2: " + roll2);
+            System.out.println("Total: " + rollTotal);
+
+            // Find out if you won
+            if (rollTotal == 7 || rollTotal == 11) {
+                System.out.println("You WON!");
+            } else {
+                if (rollTotal == 2 || rollTotal == 3 || rollTotal == 12) {
+                    System.out.println("You LOST!");
+                } else {
+                    int points = rollTotal;
+
+                    boolean reRoll = true;
+
+                    while (reRoll) {
+                        System.out.println("Press [Enter] to roll the dice");
+                        scan.nextLine();
+                        roll1 = die1.roll();
+                        roll2 = die2.roll();
+                        rollTotal = roll1 + roll2;
+                        System.out.println("Dice1: " + roll1);
+                        System.out.println("Dice2: " + roll2);
+                        System.out.println("Total: " + rollTotal);
+
+                        // Did you win?
+                        if (rollTotal == points) {
+                            System.out.println("You matched your point. You WIN!");
+                            reRoll = false;
+                        } else {
+                            if (rollTotal == 7) {
+                                System.out.println("You rolled a 7. You LOSE!");
+                                reRoll = false;
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println("Game Over!");
+            System.out.println("Wanna play again? (Y/n)");
+            String playAgain = scan.nextLine();
+            if (playAgain.equals("")) {
+
+                // Let an empty string pass
+
+            } else {
+                if (playAgain.equals("n")) {
+                    break;
+                }
+            }
+        }
+        System.out.println("Thanks for playing!");
+        scan.close();
+    }
+}
---
+++ src/crapsgame/Die.java
@@ -0,0 +1,18 @@
+package crapsgame;
+
+import java.util.Random;
+
+public class Die {
+
+    private int dieRoll;
+
+    public Die() {
+        dieRoll = 0;
+    }
+
+    public int roll() {
+        Random rand = new Random();
+        dieRoll = (int) rand.nextInt(6) + 1;
+        return dieRoll;
+    }
+}
